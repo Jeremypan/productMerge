b600bd709b08c921de158f259de56c4f719482f1
Jeremypan <phyjeremy@outlook.com>
2021-10-15 07:47:41 +1100

    Update UnitTestProcessor

--- UnitTestProcessor.py
+++ UnitTestProcessor.py
@@ -1,3 +1,4 @@
+import json
 def make_json(csvFilePath):
     import csv
     # create a dictionary
@@ -18,18 +19,27 @@ testResult=make_json("./testResult/result_output.csv")
 
 
 def unitTest(output, testResult):
-  try:
-      assert len(set([tuple(o.items()) for o in output]))==len(set([tuple(o.items()) for o in testResult]))
-      for row in output:
-        assert row in testResult
-      for row in testResult:
-        assert row in output
-  except AssertionError:
-      print("Unit Test Fail")
-  else:
-      print("Unit Test Pass")
-      
-    
-    
+      #To check if there is duplicate result at output
+      try:
+        assert len(set([tuple(o.items()) for o in output]))==len(output)
+      except AssertionError:
+        print("Unit Test Fail, Output Has duplicates")
+      #Initial check if the output has the same size as the correct results
+      try:
+        assert len(output)==len(testResult)
+      except AssertionError:
+        print("Unit Test Fail, the output size is different from the correct result")
+      finally:
+        try:
+        #To check if output has correct result
+            for row in output:
+                assert row in testResult, "Unit Test Fail, Output "+json.dumps(row)+" is the incorrect result"
+        #To check if output contain all of correct results
+            for row in testResult:
+                assert row in output, "Unit Test Fail, Output does not contain correct Result " + json.dumps(row)
+        except AssertionError as msg:
+            print(msg)
+        else:
+            print("Unit Test Pass")    
 unitTest(output,testResult)
 
